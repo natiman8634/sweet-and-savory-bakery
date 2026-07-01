48189a8a3b0df9659bd34bb6d52347e91ed35b3f
natiman8634 <natiman863400@gmail.com>
2026-07-01 16:00:01 -0700

    login and register setup

--- Back-end/back-end/src/app.ts
+++ Back-end/back-end/src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes.js';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// Basic health check route
+
+app.use(cors());
+app.use(express.json()); 
+
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Bakery API is running!');
 });
